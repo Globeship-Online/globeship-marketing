4d462f8647ca1632a628b7bc460600186f20c8f4
widgetel <info@widgetel.com>
2025-12-17 17:21:09 -0500

    Update VideoSection.tsx

--- components/VideoSection.tsx
+++ components/VideoSection.tsx
@@ -52,7 +52,7 @@ export default function VideoSection() {
             {/* YouTube iframe */}
             <iframe
               className="absolute inset-0 h-full w-full"
-              src="https://www.youtube.com/embed/WrncV_w7f20"
+              src="https://www.youtube.com/embed/ik0jbnp2ihY"
               title="Globeship Online — Your Shipping Partner"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
